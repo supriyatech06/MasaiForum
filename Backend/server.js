e596ea10028d7d87334a09e6df2d9b360c1925e1
Supriya Kumari <supriyayout@gmail.com>
2024-02-09 13:22:35 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const {connection}=require("./db");
 const { userRouter } = require('./Routes/Userroutes');
 const {postRouter} = require("./Routes/Postroutes")
 
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use("/users",userRouter)
 app.use("/posts",postRouter)
